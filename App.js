cccc5240be8c19792706260f8f6021bed11ffaa3
Jack Banning <jack1119banning@gmail.com>
2022-11-28 15:51:57 -0500

    a live location marker was added to the gps map

--- App.js
+++ App.js
@@ -10,18 +10,37 @@ import React, {useEffect, useState} from 'react';
 import StartButton from './components/StartButton';
 import EndButton from './components/EndButton';
 import Geolocation from 'react-native-geolocation-service';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 const App = () => {
   const [startingLocation, setStartLocation] = useState({});
   const [endingLocation, setEndLocation] = useState({});
   const [endingDist, setEndingDist] = useState(null);
+  const [presentLocation, setPresentLocation] = useState(null);
+
+  useEffect(() => {
+    getPresentLocation();
+  }, []);
 
   function reset() {
     setStartLocation({});
     setEndLocation({});
     setEndingDist(null);
   }
+
+  const getPresentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        console.log(latitude, longitude);
+        setPresentLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
 
   function distance(lat2, lon2) {
     const lat1 = startingLocation.latitude;
@@ -61,22 +80,31 @@ const App = () => {
         </Text>
         <View style={{flex: 2, justifyContent: 'center'}}></View>
         <View>
-          <MapView
-            mapType="satellite"
-            style={{height: '95%', width: '100%'}}
-            initialRegion={{
-              latitude: 39.6837,
-              longitude: -75.7497,
-              latitudeDelta: 0.002,
-              longitudeDelta: 0.002,
-            }}>
-            {endingDist !== null && (
-              <Text
-                style={
-                  styles.distanceText
-                }>{`You threw... ${endingDist}ft`}</Text>
-            )}
-          </MapView>
+          {presentLocation !== null && (
+            <MapView
+              mapType="satellite"
+              followsUserLocation={true}
+              style={{height: '95%', width: '100%'}}
+              initialRegion={{
+                latitude: presentLocation.latitude,
+                longitude: presentLocation.longitude,
+                latitudeDelta: 0.002,
+                longitudeDelta: 0.002,
+              }}>
+              {endingDist !== null && (
+                <Text
+                  style={
+                    styles.distanceText
+                  }>{`You threw... ${endingDist}ft`}</Text>
+              )}
+              <Marker
+                coordinate={{
+                  latitude: presentLocation.latitude,
+                  longitude: presentLocation.longitude,
+                }}
+              />
+            </MapView>
+          )}
         </View>
       </View>
 
